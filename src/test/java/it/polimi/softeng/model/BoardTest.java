8dcce0113a4bbedb1615f316af3625524f56e3da
alicecalcaterra <alice.calca2001@gmail.com>
2023-03-21 13:27:42 +0100

    Test

--- src/test/java/it/polimi/softeng/model/BoardTest.java
+++ src/test/java/it/polimi/softeng/model/BoardTest.java
@@ -12,24 +12,24 @@ class BoardTest {
         Board board = new Board();
         board.resetBoard(2);
 
-        for(int i=0;i<6;i++){
-            for(int j=0;j<5;j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 assertNull(board.getBoard()[i][j]);
             }
         }
 
         board.resetBoard(3);
 
-        for(int i=0;i<6;i++){
-            for(int j=0;j<5;j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 assertNull(board.getBoard()[i][j]);
             }
         }
 
         board.resetBoard(4);
 
-        for(int i=0;i<6;i++){
-            for(int j=0;j<5;j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 assertNull(board.getBoard()[i][j]);
             }
         }
@@ -41,10 +41,13 @@ class BoardTest {
     public void updateBoardTest(){
         Board board = new Board();
         ArrayList<Cell> positionsToBeRemoved = new ArrayList<>();
+        ArrayList<Cell> bag = new ArrayList<>();
         Cell cell = new Cell();
 
 
-        //!! TO DO: set the  tiles of the board at a value to check if it removes it correctly
+        //!! TO DO: set the  tiles of the board at a value to check if it removes it correctly, using the method from the game to initialize the bag
+
+        //board.positionTiles(bag);
         assertNotNull(board.getBoard()[1][4]);
 
         cell.setX(1);
