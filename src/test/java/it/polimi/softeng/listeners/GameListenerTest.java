b58da7948a74f38f32bfef71b3729ca08fa9fe59
Cave-8 <78547015+Cave-8@users.noreply.github.com>
2023-03-27 21:38:23 +0200

    Creation of first listener test

--- src/test/java/it/polimi/softeng/listeners/GameListenerTest.java
+++ src/test/java/it/polimi/softeng/listeners/GameListenerTest.java
@@ -1,5 +1,6 @@
 package it.polimi.softeng.listeners;
 
+import it.polimi.softeng.model.Game;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,7 @@ class GameListenerTest {
     @Test
     void testGameEvent() {
 
+        g.addListener(new GameListener());
         g.addListener(new GameListener());
         System.out.println("Player 1 calls an event...");
         g.gameEventHappens();
