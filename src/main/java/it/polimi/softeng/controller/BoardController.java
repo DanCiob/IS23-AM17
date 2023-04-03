b2c5392b21267dfddbe325ad1126561cced1cf72
alicecalcaterra <alice.calca2001@gmail.com>
2023-04-03 21:09:15 +0200

    updates: -boardController -BoardChangeNotifier

--- src/main/java/it/polimi/softeng/controller/BoardController.java
+++ src/main/java/it/polimi/softeng/controller/BoardController.java
@@ -1,9 +1,6 @@
 package it.polimi.softeng.controller;
 import it.polimi.softeng.JSONParser.PersonalCardsParser;
-import it.polimi.softeng.model.Board;
-import it.polimi.softeng.model.BoardSetter;
-import it.polimi.softeng.model.Cell;
-import it.polimi.softeng.model.Tile;
+import it.polimi.softeng.model.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -30,9 +27,9 @@ public class BoardController{
                 positionsToBeRemoved.add(temp);
             }
 
-            // call BoardSetter
-            Board board = new Board();
-            board.updateBoard(positionsToBeRemoved);
+            //TO DO: call boardsetter
+            //BoardSetter boardSetter = new Board();
+            //boardSetter.updateBoard(positionsToBeRemoved);
 
         }catch (ParseException e) {
             throw new RuntimeException(e);
