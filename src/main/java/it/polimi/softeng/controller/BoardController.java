2beb725987cea759657b63300a047fbf58b698b5
alicecalcaterra <alice.calca2001@gmail.com>
2023-04-03 14:48:33 +0200

    updates: -boardController

--- src/main/java/it/polimi/softeng/controller/BoardController.java
+++ src/main/java/it/polimi/softeng/controller/BoardController.java
@@ -1,14 +1,59 @@
 package it.polimi.softeng.controller;
+import it.polimi.softeng.JSONParser.PersonalCardsParser;
 import it.polimi.softeng.model.Cell;
+import it.polimi.softeng.model.Tile;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class BoardController {
-    private ArrayList<Cell> positionsToBeRemoved;
+    private ArrayList<Tile> positionsToBeRemoved = new ArrayList<>();
 
-    public void boardParser(String s)
-    {
-        int numoftiles = (int) s.chars().filter(x -> x == '-').count() + 1;
+    public void boardParser(String s){
+        int row, column, id;
+        String colorString;
+        Tile.TileColor color;
+        Cell temp;
+        JSONParser parser = new JSONParser();
+        positionsToBeRemoved.clear();
+        try{
+            JSONObject jsonObject = (JSONObject) parser.parse(s);
+            JSONArray boardTilesList = (JSONArray) jsonObject.get("board");
+            Iterator iterator = boardTilesList.iterator();
+
+            while(iterator.hasNext()){
+                temp = new Cell();
+
+                id = (int) jsonObject.get("id");
+                colorString = (String) jsonObject.get("color");
+                color = PersonalCardsParser.StringToColor(colorString);
+                temp.setX((int) jsonObject.get("row"));
+                temp.setY((int) jsonObject.get("column"));
+                temp.
+                positionsToBeRemoved.add(temp);
+            }
+
+        }catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
+
+
+        /*int numoftiles = (int) s.chars().filter(x -> x == '-').count() + 1;
         Cell t1 = new Cell();
         Cell t2 = new Cell();
         Cell t3 = new Cell();
@@ -45,11 +90,10 @@ public class BoardController {
                 positionsToBeRemoved.add(t1);
                 positionsToBeRemoved.add(t2);
                 positionsToBeRemoved.add(t3);
-                break;
-        }
+                break;*/
+        //}
 
     //Call to interface BoardSetter
 
 
-    }
-}
+
