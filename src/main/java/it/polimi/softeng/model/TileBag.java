e829bd1f327173d9b5d1c9d3ddf363ee52965f12
Nicolas <nicolas.carboni@mail.polimi.it>
2023-04-27 15:21:45 +0200

    Implementing TileBag

---
+++ src/main/java/it/polimi/softeng/model/TileBag.java
@@ -0,0 +1,33 @@
+package it.polimi.softeng.model;
+
+import java.util.ArrayList;
+
+import java.util.Random;
+
+import static it.polimi.softeng.Constants.*;
+
+public class TileBag {
+    private final static ArrayList<Tile> tileBag = new ArrayList<>();
+
+    public ArrayList<Tile> getTileBag() {
+        return tileBag;
+    }
+
+    public void initializeTile(){
+        tileBag.clear();
+        for(int i = 0; i < totalTiles/typesOfTiles; i+=typesOfTiles){
+            tileBag.add(new Tile(i, Tile.TileColor.WHITE));
+            tileBag.add(new Tile(i+1, Tile.TileColor.BLUE));
+            tileBag.add(new Tile(i+2, Tile.TileColor.YELLOW));
+            tileBag.add(new Tile(i+3, Tile.TileColor.PURPLE));
+            tileBag.add(new Tile(i+4, Tile.TileColor.CYAN));
+            tileBag.add(new Tile(i+5, Tile.TileColor.GREEN));
+        }
+    }
+
+    public Tile drawTile(){
+        Random random = new Random();
+        return tileBag.get(random.nextInt(tileBag.size()));
+    }
+
+}
